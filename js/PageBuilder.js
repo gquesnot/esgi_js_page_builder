05785f86fe660237da4a74f43b6e90f856f0898f
gquesnot <gaqu1994@gmail.com>
2021-12-15 18:53:33 +0100

    add config to new file

--- js/PageBuilder.js
+++ js/PageBuilder.js
@@ -1,47 +1,12 @@
 "use strict";
 
 import { HtmlElement } from './HtmlElement.js'
+import { config } from './config.js'
 
 export class PageBuilder {
   constructor () {
     this.elements = {};
-    this.elementsConfig = [
-      {
-        name: 'h1',
-        options: undefined,
-        placeholder: 'Saisir le titre principal/h1',
-        editable: true
-
-      },
-      {
-        name: 'h2',
-        options: undefined,
-        placeholder: 'Saisir le titre niveau 2/h2',
-        editable: true
-
-      },
-      {
-        name: 'h3',
-        options: undefined,
-        placeholder: 'Saisir le titre niveau 3 / h3',
-        editable: true
-
-      },
-      {
-        name: 'p',
-        options: undefined,
-        placeholder: 'Saisir le paragraphe de text',
-        editable: true
-
-      },
-      {
-        name: 'hr',
-        options: undefined,
-        placeholder: '',
-        editable: false
-
-      }
-    ];
+    this.elementsConfig = config
     this.contentElem = document.getElementById('content');
     this.btnExport = document.getElementById('export');
     this.popup = document.getElementById('popup');
---
+++ js/config.js
@@ -0,0 +1,37 @@
+export const config = [
+  {
+    name: 'h1',
+    options: undefined,
+    placeholder: 'Saisir le titre principal/h1',
+    editable: true
+
+  },
+  {
+    name: 'h2',
+    options: undefined,
+    placeholder: 'Saisir le titre niveau 2/h2',
+    editable: true
+
+  },
+  {
+    name: 'h3',
+    options: undefined,
+    placeholder: 'Saisir le titre niveau 3 / h3',
+    editable: true
+
+  },
+  {
+    name: 'p',
+    options: undefined,
+    placeholder: 'Saisir le paragraphe de text',
+    editable: true
+
+  },
+  {
+    name: 'hr',
+    options: undefined,
+    placeholder: '',
+    editable: false
+
+  }
+]
